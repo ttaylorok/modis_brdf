26b123ddfecc1067a8c9063ceab0336960914c53
ttaylorok <ttaylor.ok@gmail.com>
2020-08-27 11:06:32 -0500

    added nadir correction

--- modis_brdf3.js
+++ modis_brdf3.js
@@ -53,11 +53,11 @@ var cloudsRemoved = collection
 //var collection2 = cloudsRemoved.map(function(image){
 //  return image.clip(roi3)
 //});
-Map.addLayer(cloudsRemoved,{},'cloudsRemoved')
+//Map.addLayer(cloudsRemoved,{},'cloudsRemoved')
 
 var numPixels = cloudsRemoved.count();
 var newMask = numPixels.select('sur_refl_b01').gt(10);
-Map.addLayer(numPixels,{},'numPixels');
+//Map.addLayer(numPixels,{},'numPixels');
 
 var maskFew = function(image){
   return image.updateMask(newMask);
@@ -67,10 +67,10 @@ var masked = cloudsRemoved.map(maskFew);
 //Map.addLayer(masked, {}, 'masked');
 
 var withKernels = masked.map(addKernels); //CHANGE BACK TO MASKED or MASKEDMODIS
-Map.addLayer(withKernels.select(['Ksparse','Kvol']), {}, 'withKernels');
+//Map.addLayer(withKernels.select(['Ksparse','Kvol']), {}, 'withKernels');
 
 var countKernels = withKernels.count();
-Map.addLayer(countKernels, {}, 'countKernels');
+//Map.addLayer(countKernels, {}, 'countKernels');
 
 // Convert to an array. Give the axes names for more readable code.
 var array = withKernels.toArray();
@@ -84,15 +84,67 @@ var fit = x.matrixSolve(y);
 print(fit);
 Map.addLayer(fit,{},'fit');
 Map.addLayer(x,{},'x');
-Map.addLayer(y,{},'y');
+//Map.addLayer(y,{},'y');
 
 var mm = x.matrixMultiply(fit);
 print('mm',mm);
-//Map.addLayer(mm,{},'mm');
+Map.addLayer(mm,{},'mm');
 
 var fiso = fit.arrayGet([0, 0]);
 var fvol = fit.arrayGet([1, 0]);
 var fgeo = fit.arrayGet([2, 0]);
+
+var params = ee.ImageCollection('MODIS/006/MCD43A1')
+  .filterDate('2018-08-01', '2018-12-01')
+  .filterBounds(roi3);
+  
+//Map.addLayer(params.median(), {}, 'params');
+
+// function
+// calc solar noon zenith
+// calc kernels
+// addBands(ones, kvol, ksparse)
+// map function to collection
+
+var lat = ee.Image.pixelLonLat().select('latitude').multiply(Math.PI).divide(180);
+
+var calcNadir = function(image){
+  var date = ee.Date(image.get('system:time_start'));
+  var year = date.get('year');
+  var yearStart = ee.Date.fromYMD(year,1,1);
+  var days = date.difference(yearStart,'day').toFloat();
+  
+  var d = ((((ee.Image(days).subtract(173)).multiply(0.98563*Math.PI/180))
+    .cos()).multiply(0.39795)).asin().rename('d'); // solar declination
+  var sn = lat.subtract(d).rename('sn'); // zenith at solar noon
+  
+  return image.addBands([d,sn])
+}
+
+var test_z = collection.map(calcNadir);
+Map.addLayer(test_z,{},'zenith')
+
+var kernels_nadir = brdf.calcKernel(ee.Image(0),
+  ee.Image(0), ee.Image(0), ee.Image(0));
+  
+var nadir_matrix = ee.ImageCollection(ee.Image(1)
+  .addBands(kernels_nadir.select(['Kvol', 'Ksparse'])))
+  .toArray()
+  //.arrayTranspose(1,0)//.arrayReshape(ee.Image(3).toArray(),2)
+
+//var fit2 = fit.arrayReshape(ee.Image(3).toArray(),1)
+  
+//var as = nadir_matrix.arraySlice(1,0,2)
+
+
+var reflectance = nadir_matrix.matrixMultiply(fit).arrayGet([0,0])
+
+Map.addLayer(nadir_matrix,{},'nadir_matrix')
+//Map.addLayer(fit2,{},'fit2')
+//Map.addLayer(as,{},'as')
+Map.addLayer(reflectance,{},'reflectance')
+  
+
 
 //Map.addLayer(fiso, {}, 'fiso');
 //Map.addLayer(fvol, {}, 'fvol');
