1ffeba9c1a73198f68a2645ab3abd12fc17f9005
ttaylorok <ttaylor.ok@gmail.com>
2020-08-30 00:38:33 -0500

    more qc

--- modis_brdf3.js
+++ modis_brdf3.js
@@ -1,15 +1,16 @@
 var brdf = require('users/ttaylorok/amazon:modis_kernels');
 
 var START_DATE = '01-01'// MM-DD
-var END_DATE = '04-01' // MM-DD
-var YEARS = ['2018','2019']
+var END_DATE = '02-01' // MM-DD
+var YEARS = ['2018']
 var COMBINED = true // combine terra dna aqua
 var MIN_SAMPLES = 10
+var PERCENTILE_FILT = [5,95]
 
 var getData = function(start, end, year, combined){
-  var c = ee.ImageCollection('MODIS/006/MYD09GA');
+  var c = ee.ImageCollection('MODIS/006/MOD09GA');
   if(COMBINED){
-    c = c.merge('MODIS/006/MOD09GA')
+    c = c.merge('MODIS/006/MYD09GA')
   }
   return c.filterDate(year+'-'+start, year+'-'+end);
 }
@@ -26,6 +27,7 @@ var qcBits = function(image){
   var shadowBitMask = 4;
   var adjacentBitMask = 1 << 13;
   var cloudInternalBitMask = 1 << 10;
+  var snowBitMask = 1 << 15;
 
   var qa = image.select('state_1km');
 
@@ -33,9 +35,23 @@ var qcBits = function(image){
   var shadowMask = qa.bitwiseAnd(shadowBitMask).eq(0).rename('shadowMask');
   var adjacentMask = qa.bitwiseAnd(adjacentBitMask).eq(0).rename('adjacentMask');
   var cloudInternalMask = qa.bitwiseAnd(cloudInternalBitMask).eq(0).rename('cloudInternalMask');
+  var snowMask = qa.bitwiseAnd(snowBitMask).eq(0).rename('snowMask');
   //var mask = cloudMask.and(shadowMask).and(adjacentMask).and(cloudInternalMask);
+  
+  var qc500 = image.select('QC_500m');
+  
+  var qaBitMask = 3;
+  var atmBitMask = 1 << 30;
+  var qab1BitMask = 60;
+  
+  var qaMask = qc500.bitwiseAnd(qaBitMask).eq(0).rename('qaMask');
+  var atmMask = qc500.bitwiseAnd(atmBitMask).eq(0).rename('atmMask');
+  var qab1Mask = qc500.bitwiseAnd(qab1BitMask).eq(0).rename('qab1Mask');
+  
+  var totalMask = shadowMask.and(adjacentMask).and(cloudInternalMask).and(snowMask).rename('totalMask');
 
-  return image.addBands([cloudMask,shadowMask,adjacentMask,cloudInternalMask]);
+  return image.addBands([cloudMask,shadowMask,adjacentMask,cloudInternalMask,
+    qaMask, atmMask, qab1Mask, totalMask, snowMask]);
 }
 
 var collectionQC = collection.map(qcBits);
@@ -44,12 +60,24 @@ var visParams = {bands : ['sur_refl_b01', 'sur_refl_b04', 'sur_refl_b03'],
                 min : 0, max : 2000};
 
 // 2018-01-27 has two images
-Map.addLayer(collection.filterDate('2018-01-27').first(),visParams,'collection');
+Map.addLayer(collection,visParams,'collection');
+//.filterDate('2018-01-27')
+// point -94.95904983402565,34.621335231913854
+// months 01-04 2018
+//.first()
 //  .filterMetadata('system:index','equals',0)
 //.reduce(ee.Reducer.last())
-Map.addLayer(collectionQC.filterDate('2018-01-27').first(),visParams,'collectionQC');
+Map.addLayer(collectionQC,{bands:['qab1Mask'],min:0,max:1},'collectionQC');
+//.filterDate('2018-01-27')
+//.first()
 //.reduce(ee.Reducer.last())
 //var collection = collection2018//.merge(collection2019).sort('system:time_start');
+
+var chart_qc = ui.Chart.image.doySeries(
+  collectionQC.select(['qaMask']),
+  mypoint, ee.Reducer.first(),10,ee.Reducer.first())
+  .setChartType('ScatterChart')
+//print(chart_qc)
 
 var corrected = ee.ImageCollection('MODIS/006/MCD43A4')
   .filterDate('2018-01-01','2018-04-01');
@@ -60,58 +88,92 @@ Map.addLayer(corrected.median(),
   },'corrected')
 
 
-             
-
-
 var lat = ee.Image.pixelLonLat().select('latitude').multiply(Math.PI/180);
 
 // mask clouds
 function maskMODIS(image) {
-  var cloudBitMask = 3;
   var shadowBitMask = 4;
   var adjacentBitMask = 1 << 13;
   var cloudInternalBitMask = 1 << 10;
+  var snowBitMask = 1 << 15;
 
   var qa = image.select('state_1km');
+  
+  //var valMask_red = image.select('sur_refl_b01').lt(1200)
+  //var valMask_green = image.select('sur_refl_b04').lt(1200)
+  //var valMask_blue = image.select('sur_refl_b03').lt(1200)
 
-  var cloudMask = qa.bitwiseAnd(cloudBitMask).eq(0);
   var shadowMask = qa.bitwiseAnd(shadowBitMask).eq(0);
   var adjacentMask = qa.bitwiseAnd(adjacentBitMask).eq(0);
   var cloudInternalMask = qa.bitwiseAnd(cloudInternalBitMask).eq(0);
-  var mask = cloudMask.and(shadowMask).and(adjacentMask).and(cloudInternalMask);
+  var snowMask = qa.bitwiseAnd(snowBitMask).eq(0);
+  var mask = shadowMask
+    .and(adjacentMask)
+    .and(cloudInternalMask)
+    .and(snowMask)
+    //.and(valMask_red)
+    //.and(valMask_green)
+    //.and(valMask_blue);
 
-  return image.updateMask(mask)//.clip(roi)
-      //.copyProperties(image, ["system:time_start"]);
+  return image.updateMask(mask)
 }
+
 
 
 var cloudsRemoved = collection.map(maskMODIS);
   //.map(function(image){return image.clip(roi3)})
   
-  
-//Map.addLayer(cloudsRemoved.median(),visParams,'cloudsRemoved')
+var percentiles = cloudsRemoved
+  .select(['sur_refl_b01','sur_refl_b04','sur_refl_b03'])
+  .reduce(ee.Reducer.percentile(PERCENTILE_FILT))
 
-var numPixels = cloudsRemoved.count();
+Map.addLayer(percentiles,{},'percentiles')
+
+var filtPercentiles = function(image){
+  var redMask = (image.select('sur_refl_b01')
+    .gte(percentiles.select('sur_refl_b01_p5')))
+    .and(image.select('sur_refl_b01')
+    .lte(percentiles.select('sur_refl_b01_p95')));
+  var greenMask = (image.select('sur_refl_b04')
+    .gte(percentiles.select('sur_refl_b04_p5')))
+    .and(image.select('sur_refl_b04')
+    .lte(percentiles.select('sur_refl_b04_p95')));
+  var blueMask = (image.select('sur_refl_b03')
+    .gte(percentiles.select('sur_refl_b03_p5')))
+    .and(image.select('sur_refl_b03')
+    .lte(percentiles.select('sur_refl_b03_p95')));
+    
+  return image.updateMask(redMask.and(greenMask).and(blueMask))
+}
+
+var pFilt = cloudsRemoved.map(filtPercentiles)
+
+//Map.addLayer(pFilt.median(),visParams,'pFilt')
+
+var numPixels = pFilt.count();
 var newMask = numPixels.select('sur_refl_b01').gt(MIN_SAMPLES);
 var maskFew = function(image){
   return image.updateMask(newMask);
 }
 
-var masked = cloudsRemoved.map(maskFew);
-Map.addLayer(masked.median(),visParams,'masked_median')
+var masked = pFilt.map(maskFew);
+var masked_median = masked.median()
+
+
+Map.addLayer(masked,visParams,'masked')
 //Map.addLayer(masked.last(),visParams,'masked_last')
 
 var visParams2 = {bands:['red_nadir','green_nadir','blue_nadir'], min:0, max:0.2}
-Map.addLayer(refl5, visParams2, 'ref4')
+Map.addLayer(refl8, visParams2, 'ref8')
 
 var addKernels = function(image){
-  // calculate kernals
   var theta_i = image.select('SolarZenith').multiply(0.01);
   var theta_v = image.select('SensorZenith').multiply(0.01);
   var phi_i = image.select('SolarAzimuth').multiply(0.01);
   var phi_v = image.select('SensorAzimuth').multiply(0.01);
   var phi_r = phi_i.subtract(phi_v); 
   
+  // calculate kernels at acquired angles
   var kernels = brdf.calcKernel(theta_i,theta_v, phi_i, phi_v)
     .select(['Kvol','Ksparse']);
   
@@ -129,10 +191,12 @@ var addKernels = function(image){
   var kernels_nadir = brdf.calcKernel(sn,
     ee.Image(0), ee.Image(0), ee.Image(0))
     .select(['Kvol','Ksparse']).rename(['Kvol_nadir','Ksparse_nadir']);
-    
+  
+  // constant for fiso
   var ones = ee.Image(1).rename('ones');
   
-  return image.addBands(kernels).addBands(kernels_nadir).addBands(ones);
+  return image.addBands(kernels).addBands(kernels_nadir).addBands(ones)
+    .addBands([d.multiply(180/Math.PI),sn]);
 };
 
 
@@ -146,6 +210,7 @@ var withKernels = masked.map(addKernels);
 var imageAxis = 0;
 var bandAxis = 1;
 
+// perform inversion
 var red = withKernels.select(['sur_refl_b01','ones','Kvol','Ksparse']).toArray();
 var x_red = red.arraySlice(bandAxis, 1, 4);
 var y_red = red.arraySlice(bandAxis, 0, 1).divide(10000);
@@ -214,13 +279,27 @@ var chart1 = ui.Chart.image.doySeries(
   reflectance.select('red_orig','red_nadir'),
   mypoint, ee.Reducer.first(),10,ee.Reducer.first())
   .setChartType('ScatterChart')
-print(chart1)
+//print(chart1)
+
+var scaleAngles = function(image){
+  return image.addBands([
+    image.select('SolarZenith').divide(100).rename('SolarZenith_scaled'),
+    image.select('SensorZenith').divide(100).rename('SensorZenith_scaled'),
+    image.select('SolarAzimuth').divide(100).rename('SolarAzimuth_scaled'),
+    image.select('SensorAzimuth').divide(100).rename('SensorAzimuth_scaled'),
+    image.select('sur_refl_b01').divide(10000).rename('sur_refl_b01_scaled'),
+    image.select('sur_refl_b04').divide(10000).rename('sur_refl_b04_scaled'),
+    image.select('sur_refl_b03').divide(10000).rename('sur_refl_b03_scaled')
+  ]);
+}
+var scaled = reflectance.map(scaleAngles)
 
 var chart1a = ui.Chart.image.doySeries(
-  reflectance.select('SolarZenith','SensorZenith','SolarAzimuth','SensorAzimuth'),
-  mypoint, ee.Reducer.first(),10,ee.Reducer.first())
-  .setChartType('ScatterChart')
-print(chart1a)
+  scaled.select('SolarZenith_scaled',
+  'SensorZenith_scaled','SolarAzimuth_scaled','SensorAzimuth_scaled'),
+  mypoint, ee.Reducer.first(),10,ee.Reducer.first(),0,365)
+  .setChartType('ScatterChart').setOptions({pointSize: 3});
+//print(chart1a)
 
 //var pix = reflectance.select('red_orig').getRegion(mypoint, 10);
 //print('pix',pix.slice(1,-1).toArray())
@@ -239,18 +318,31 @@ var chart1b = ui.Chart.array.values(yp.toArray(),0,xp.toArray()).setOptions({
       vAxis: {'title': 'sur_refl_b01'},
       pointSize: 5,
 });
-print(chart1b);
+//print(chart1b);
+
+var chart1c = ui.Chart.image.doySeries(
+  reflectance.select('sn','d'),
+  mypoint, ee.Reducer.first(),10,ee.Reducer.first())
+  .setChartType('LineChart')
+//print(chart1c)
+
+var chart1d = ui.Chart.image.doySeries(
+  scaled.select('sur_refl_b01_scaled','red_nadir'),
+  mypoint, ee.Reducer.first(),10,ee.Reducer.first())
+  .setChartType('ScatterChart').setOptions({pointSize:3})
+print(chart1d)
+
 
 var chart2 = ui.Chart.image.doySeries(
-  reflectance.select('green_orig','green_nadir'),
+  scaled.select('sur_refl_b04_scaled','green_nadir'),
   mypoint, ee.Reducer.first(),10,ee.Reducer.first())
-  .setChartType('ScatterChart')
+  .setChartType('ScatterChart').setOptions({pointSize:3})
 print(chart2)
 
 var chart3 = ui.Chart.image.doySeries(
-  reflectance.select('blue_orig','blue_nadir'),
+  scaled.select('sur_refl_b03_scaled','blue_nadir'),
   mypoint, ee.Reducer.first(),10,ee.Reducer.first())
-  .setChartType('ScatterChart')
+  .setChartType('ScatterChart').setOptions({pointSize:3})
 print(chart3)
 
 chart1.onClick(function(xValue, yValue, seriesName) {
@@ -260,15 +352,17 @@ chart1.onClick(function(xValue, yValue, seriesName) {
   var image1 = ee.Image(collection.filterDate(equalDate).first());
   var image2 = ee.Image(masked.filterDate(equalDate).first());
   var image3 = ee.Image(reflectance.filterDate(equalDate).first());
+  var image4 = ee.Image(collectionQC.filterDate(equalDate).first());
   var layer1 = ui.Map.Layer(image1, visParams,'collection');
   var layer2 = ui.Map.Layer(image2, visParams,'masked');
   var layer3 = ui.Map.Layer(image3,
     {bands:['red_orig','green_orig','blue_orig'],min:0,max:0.2},'reflectance');
-  Map.layers().reset([layer1,layer2,layer3]);
+  var layer4 = ui.Map.Layer(image4,imageVisParam2,'collectionQC');
+  Map.layers().reset([layer1,layer2,layer3,layer4]);
 });
 
 Export.image.toAsset(reflectance.median(),
-  "reflectance_ok_jan-march_18-19_orig_proj","reflectance_ok_jan-march_18-19",null,null,roi,500,proj.crs())
+  "reflectance_region_jan_18_pfilt","reflectance_region_jan_18_pfilt",null,null,roi,500,proj.crs())
 
 //Map.addLayer(fiso, {}, 'fiso');
 //Map.addLayer(fvol, {}, 'fvol');
